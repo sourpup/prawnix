eb69447221567b1b6249e3ae7336e45316840dfd
Eva Emmerich <eva@evaemmerich.com>
2025-12-18 10:04:23 +0100

    sway/mistral: set external monitor wallpaper

--- modules/sway/mistral.nix
+++ modules/sway/mistral.nix
@@ -15,14 +15,16 @@ let
 # You can get the names of your outputs by running: swaymsg -t get_outputs
 
 
-# resolution and scaling
+# Default resolution and scaling, wallpaper
 output * scale 1.5
+output * bg /etc/wallpapers/horizontal/cat_window.png fill
 
+# monitor specific configurations
 output DP-1 resolution 7680x2160@59.9877Hz
 output DP-1 scale 1.0
+output DP-1 bg /etc/wallpapers/horizontal/32_10_house_island.jpg fill
 
 # Default wallpaper
-output * bg /etc/wallpapers/horizontal/cat_window.png fill
 
     '';
   });
