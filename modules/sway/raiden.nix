3d68c720e98d34f5503d6e47fb8e093799dcd77f
Eva Emmerich <eva@evaemmerich.com>
2026-01-06 17:50:25 +0100

    raiden: add kanshi config

--- modules/sway/raiden.nix
+++ modules/sway/raiden.nix
@@ -21,13 +21,32 @@ output * bg /etc/wallpapers/horizontal/grove_city.png fill
 output * scale 1.0
 
 # External monitors
-output DP-1 resolution 7680x2160@59.987Hz
-output DP-1 dpms on
-output DP-1 scale 1.2
-output DP-1 bg /etc/wallpapers/horizontal/future_stacked_city.png fill
+output DP-7 resolution 7680x2160@59.987Hz
+output DP-7 scale 1.2
+output DP-7 dpms on
+output DP-7 bg /etc/wallpapers/horizontal/future_stacked_city.png fill
 
     '';
   });
+
+  host_kanshi_conf = (pkgs.writeTextFile {
+    name = "host_kanshi_conf";
+    text = ''
+
+    profile docked {
+      output "DP-7" enable
+      output "eDP-1" disable
+    }
+
+    profile undocked {
+      output "eDP-1" enable
+      output "DP-7" disable
+    }
+
+    '';
+
+  });
+
 in
 
 {
@@ -43,6 +62,25 @@ in
         name = "sway.conf";
         files = [ ./sway.conf host_sway_conf ];
       });
+  };
+
+  # install this hosts kanshi config
+  environment.etc = {
+    "xdg/kanshi/config".source = ( host_kanshi_conf );
+  };
+
+  # start kanshi on login
+  systemd.user.services.kanshi = {
+    wantedBy = [ "sway-session.target" ];
+    description = "Kanshi - display auto config";
+    partOf = [ "sway-session.target" ];
+    after = [ "sway-session.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.kanshi}/bin/kanshi --config /etc/xdg/kanshi/config";
+      Restart = "always";
+    };
   };
 
 }
