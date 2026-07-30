de1c015203a2fefe782d2a9fcb99ce7ffb4d7317
Eva Emmerich <eva@evaemmerich.com>
2026-07-30 20:55:20 +0200

    tofi: clear cache file on activation

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -197,6 +197,18 @@ in
     "xdg/tofi/tofi-config".source = ./tofi-config;
   };
 
+  # tofi fails to regenerate its cache properly on nixos
+  # https://github.com/philj56/tofi/issues/115
+  system.userActivationScripts.regenerateTofiCache.text =
+  ''
+    if [[ -d $XDG_CACHE_HOME ]]; then
+      tofi_cache=$XDG_CACHE_HOME/tofi-drun
+    else
+      tofi_cache=$HOME/.cache/tofi-drun
+    fi
+    [[ -f "$tofi_cache" ]] && rm "$tofi_cache"
+  '';
+
   fonts.packages = with pkgs; [
     cantarell-fonts
     dejavu_fonts
